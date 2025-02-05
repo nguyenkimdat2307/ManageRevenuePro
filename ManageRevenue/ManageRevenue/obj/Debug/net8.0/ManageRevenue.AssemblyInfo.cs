20abbdf03a012c94e922c4846ea69ab5d6b59782
DESKTOP-CEHRP21\kimda <nguyenkimdat2307@gmail.com>
2025-02-05 09:39:59 +0700

    create project

--- ManageRevenue/ManageRevenue/obj/Debug/net8.0/ManageRevenue.AssemblyInfo.cs
+++ ManageRevenue/ManageRevenue/obj/Debug/net8.0/ManageRevenue.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManageRevenue")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cec9d7d427b4d94b864150d515b272e741b2847")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManageRevenue")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManageRevenue")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
